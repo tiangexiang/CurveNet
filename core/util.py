ef4328381ab76999b20a8a90ae6407f894281d3d
skylerroh <skylerroh@gmail.com>
2021-11-03 02:16:56 +0000

    working base training

--- core/util.py
+++ core/util.py
@@ -26,13 +26,14 @@ def cal_loss(pred, gold, smoothing=True):
         n_lab = torch.sum(one_hot,dim=1)
 
         # one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
-        one_hot = (one_hot * (1 - eps)) + ((1 - one_hot) * eps / n_lab[:, None])
+        one_hot = (one_hot * (1 - eps/(n_lab[:, None]))) + \
+            ((1 - one_hot) * eps / (n_class - n_lab)[:, None])
         
         log_prb = F.log_softmax(pred, dim=1)
 
         loss = -(one_hot * log_prb).sum(dim=1).mean()
     else:
-        loss = F.cross_entropy(pred, gold, reduction='mean')
+        loss = F.multilabel_soft_margin_loss(pred, gold)
 
     return loss
 
